674b92c59c97337568136c92c1e26c218b6f4179
Leonardo Damian Suarez <suarezldamian@gmail.com>
2022-06-27 16:15:39 -0300

    Code cleaning, logic corrections.

--- src/logic.js
+++ src/logic.js
@@ -18,59 +18,34 @@ export async function orderValues(blocks, token) {
     let orderedBlocks = [];
     var requestsCount = 0;
 
+    orderedBlocks.push(blocks[0]);
+
     console.log("El array inicial es: ", blocks);
     console.log("Ordenando bloques...");
 
     for (let i = 0; i < blocks.length; i++) {        
         for(let j = 0; j < blocks.length; j++) {
             
-            let response;
-
-            if(orderedBlocks.length == 0) {
-                response = await fetch(`https://rooftop-career-switch.herokuapp.com/check?token=${token}`, {
-                    method: "POST",
-                    headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            "blocks": [
-                                blocks[j],
-                                blocks[j+1]
-                            ]
-                        }
-                    )
-                })
-            }
-            else {
-                response = await fetch(`https://rooftop-career-switch.herokuapp.com/check?token=${token}`, {
-                    method: "POST",
-                    headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            "blocks": [
-                                orderedBlocks[orderedBlocks.length-1],
-                                blocks[j]
-                            ]
-                        }
-                    )
-                })
-            }            
+            const response = await fetch(`https://rooftop-career-switch.herokuapp.com/check?token=${token}`, {
+                method: "POST",
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(
+                    {
+                        "blocks": [
+                            orderedBlocks[orderedBlocks.length-1],
+                            blocks[j]
+                        ]
+                    }
+                )
+            });
     
             let result = await response.json();
 
             if(result.message === true) {
-                if(orderedBlocks.length == 0) {
-                    orderedBlocks.push(blocks[j]);
-                    orderedBlocks.push(blocks[j+1]);
-                }
-                else{
-                    orderedBlocks.push(blocks[j]);
-                }
+                orderedBlocks.push(blocks[j]);
             }
 
             requestsCount = requestsCount + 1;
@@ -78,8 +53,8 @@ export async function orderValues(blocks, token) {
         console.log(`Iteración: ${i+1}, Array ordenado: ${orderedBlocks}`);
     }
 
-    console.log("Cantidad de peticiones: ", requestsCount);
-    console.log("El array ordenado es:", orderedBlocks);
+    console.log("Cantidad de peticiones realizadas: ", requestsCount);
+    console.log("El array ordenado es: ", orderedBlocks);
 
     return orderedBlocks;
 }
@@ -99,7 +74,7 @@ export async function check(orderedBlocks, token) {
                 "encoded": encodedString
             }
         )
-    })
+    });
 
     const result = await response.json();
 
